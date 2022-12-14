e4162665e45bd6b0d130d3f5220af3eeb97f00f7
Maryia Ondra <maryiaondra@gmail.com>
2022-12-14 23:28:11 +0400

    Add vertical search

--- Assets/Scripts/WordOrganizer.cs
+++ Assets/Scripts/WordOrganizer.cs
@@ -7,15 +7,22 @@ namespace WordAlgorithm
 {
     public class WordOrganizer
     {
-        private Dictionary<string, List<LetterConfig>> lettersConfigs = new();
+        private const int MIN_WORD_LENGTH = 3;
+
+        private Dictionary<string, List<LetterConfig>> lettersConfigs;
+        
         public void OrganizeWordList(List<List<string>> gridConfig)
         {
+            lettersConfigs = new Dictionary<string, List<LetterConfig>>();
+            
             FindHorizontalWords(gridConfig);
-            //FindVerticalWords(gridConfig);
+            FindVerticalWords(gridConfig);
+            
             Debug.Log("KEYS: " +  lettersConfigs.Keys.Count);
+            
             foreach (var key in lettersConfigs.Keys)
             {
-              Debug.Log($"Horizontal words: {key}");  
+              Debug.Log($"Words: {key}");  
             }
         }
         
@@ -29,26 +36,30 @@ namespace WordAlgorithm
                 for (int j = 0; j < gridConfig[i].Count; j++)
                 {
                     var trimmedLetter =  gridConfig[i][j].Trim();
-                    
-                    Debug.Log($"Trimmed letter words: [{trimmedLetter}]");  
-                    
+                   
                     if (trimmedLetter.IsEmpty())
                     {
-                        if (newWord.Length < 3)
+                        if (!newWord.IsEmpty() && (newWord.Length < MIN_WORD_LENGTH))
                         {
+                            Debug.Log("CLEAR");
                             newLettersConfigs.Clear();
                             newWord = String.Empty;
                         }
+                        Debug.Log("CONTINUE");
                         continue;
                     }
-                    
-                    Debug.Log($"Add Trimmed letter to newLettersConfigs: [{trimmedLetter}]");  
+
+
                     LetterConfig newLetter = new LetterConfig( trimmedLetter,i, j);
                     newLettersConfigs.Add(newLetter);
+                    Debug.Log("ADD LETTER:" + newLetter);
                     newWord += trimmedLetter;
+                    Debug.Log("ADD WORD LETTER:" + newWord);
                 }
+                
+                Debug.Log("NEW WORD:" + newWord);
 
-                if (newLettersConfigs.Count >= 3)
+                if (newWord.Length >= MIN_WORD_LENGTH)
                 {
                     lettersConfigs.Add(newWord, newLettersConfigs);
                 }
@@ -57,12 +68,40 @@ namespace WordAlgorithm
 
         private void FindVerticalWords(List<List<string>> gridConfig)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < gridConfig.Count; i++)
+            {
+                List<LetterConfig> newLettersConfigs = new List<LetterConfig>();
+                string newWord = String.Empty;
+                
+                for (int j = 0; j < gridConfig.Count; j++)
+                {
+                    var trimmedLetter = gridConfig[j][i].Trim();
+                    
+                    if (trimmedLetter.IsEmpty())
+                    {
+                        if (newWord.Length < MIN_WORD_LENGTH)
+                        {
+                            newLettersConfigs.Clear();
+                            newWord = String.Empty;
+                        }
+                        continue;
+                    }
+
+                    LetterConfig newLetter = new LetterConfig( trimmedLetter,i, j);
+                    newLettersConfigs.Add(newLetter);
+                    newWord += trimmedLetter;
+                }
+                
+                if (newWord.Length >= MIN_WORD_LENGTH)
+                {
+                    lettersConfigs.Add(newWord, newLettersConfigs);
+                }
+            }
         }
     }
-
-    public class LetterConfig
-    {
+    
+    public struct LetterConfig
+    { 
         public string Letter { get; } 
         public int RowIndex{ get; } 
         public int ColumnIndex{ get; } 
